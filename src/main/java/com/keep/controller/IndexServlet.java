3b03e6470809d90776f3762189abf8243d72b52f
VictoriaYosufiv <victoriashologon@gmail.com>
2018-06-08 20:08:16 +0300

    версія на 08.06

--- src/main/java/com/keep/controller/IndexServlet.java
+++ src/main/java/com/keep/controller/IndexServlet.java
@@ -26,10 +26,10 @@ public class IndexServlet extends HttpServlet {
                 indView.print(response, "About", indView.readHtmlFile("about"));
                 break;
             case "/profile":
-                indView.print(response, "Profile", indView.readHtmlFile("user-form"));
+                indView.print(response, "Profile", indView.readHtmlFile("sign-up"));
                 break;
             case "/signup":
-                indView.print(response, "Signup", indView.readHtmlFile("user-form"));
+                indView.print(response, "Signup", indView.readHtmlFile("sign-up"));
                 break;
             case "/welcome":
                 indView.print(response, "Welcome", indView.readHtmlFile("about"));
